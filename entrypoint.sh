864157bbe087bdcb3d8236ee82a0dd67f011330f
Mohammad Hosseini <mohammad.hoseyny@gmail.com>
2024-12-31 21:02:33 +0330

    fix: increase timeout

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,5 @@ if [ "$POLLING" = "true" ]; then
     python app.py
 else
     echo "POLLING is not true. Starting Gunicorn server..."
-    gunicorn -w 4 -b 0.0.0.0:8443 'app:app'
+    gunicorn -w 4 --timeout 1000 -b 0.0.0.0:8443 'app:app'
 fi
